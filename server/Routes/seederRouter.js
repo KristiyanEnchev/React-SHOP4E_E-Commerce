bae1b24394a706d9316d539422322ad3285e4676
Kiko <kristiqnenchevv@gmail.com>
2022-08-02 23:56:16 +0300

    rem

--- server/Routes/seederRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as controller from '../Controllers/seederController.js';
-
-const seedRouter = express.Router();
-
-seedRouter.get('/', controller.seeder);
-
-export default seedRouter;
